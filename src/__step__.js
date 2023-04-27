57d3ee9dbdc8e1bd0eb6931ab1dd29eb950e88b9
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-04-27 15:58:17 +0600

    signup

---
+++ src/__step__.js
@@ -0,0 +1,25 @@
+  
+  
+  
+  
+  /*Hoisting*
+   * one time per pc
+   * 1.npm install -g firebase tools
+   * 2.firebase loging
+   * For each project one time
+   * 1.firebase init
+   * 2.proced
+   * 3.hoisting:firebase up and down arrow
+   * 4.exiting project
+   * 5.select the project careful
+   * 6.which project as public directory : dist
+   * 7.single page application
+   * 8.continus deployment 
+   * 
+   * For every time deploy
+   * npm run build
+   * firebase deploy
+   * 
+   * 
+   * LIVE LINK: https://emajhon-with-firebaseauth.web.app
+   */
