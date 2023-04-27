94956af2b42faa50fba60a38affad558c294f46f
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-04-27 15:58:17 +0600

    signup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,6 +5,7 @@ import { AuthContext } from '../SignUp/AuthProvider/AuthProvider';
 
 
 const Login = () => {
+  const [show, setShow] = useState(false)
   const { signIn} = useContext(AuthContext)
  const [success, setSuccess] = useState('')
   const navigate = useNavigate();
@@ -48,8 +49,13 @@ const from = location.state?.from?.pathname || '/';
         </div>
         <div className="mb-6">
           <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-          <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+          <input type={show ? 'text':"password"} id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
         </div>
+            <p onClick={() => setShow(!show)}><small>
+              {
+                show ? <span>hide password</span>: <span>show password</span>
+              }
+              </small></p>
         <p className='mb-6'>New to ema jhon?  <small><Link to="/signup">Sign Up</Link></small></p>
 
 
